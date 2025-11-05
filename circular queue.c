2f0273f9d5c9b6961328d2928631fc473ac94423
sucharitacs24 <sucharita.cs24@bmsce.ac.in>
2025-11-05 13:16:40 +0530

    Add files via upload

---
+++ circular queue.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
+
+int queue[SIZE];
+int front = -1, rear = -1;
+
+int isFull() {
+    return (front == (rear + 1) % SIZE);
+}
+
+int isEmpty() {
+    return (front == -1);
+}
+
+void enqueue(int value) {
+    if (isFull()) {
+        printf("Queue is full\n");
+        return;
+    }
+    if (isEmpty()) {
+        front = rear = 0;
+    } else {
+        rear = (rear + 1) % SIZE;
+    }
+    queue[rear] = value;
+    printf("Inserted %d\n", value);
+}
+
+void dequeue() {
+    if (isEmpty()) {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Deleted %d\n", queue[front]);
+    if (front == rear) {
+        front = rear = -1;
+    } else {
+        front = (front + 1) % SIZE;
+    }
+}
+
+void display() {
+    if (isEmpty()) {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Queue elements: ");
+    int i = front;
+    while (1) {
+        printf("%d ", queue[i]);
+        if (i == rear) break;
+        i = (i + 1) % SIZE;
+    }
+    printf("\n");
+}
+
+int main() {
+    int choice, value;
+    while (1) {
+        printf("\n--- Circular Queue Menu ---\n");
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("Enter value to insert: ");
+            scanf("%d", &value);
+            enqueue(value);
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
